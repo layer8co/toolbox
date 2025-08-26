23569221d79038cc63f17119326ff617ca0ff95e
koonix <me@koonix.org>
2025-08-26 14:01:48 +0330

    add package "ioutil"

---
+++ ioutil/footer_reader.go
@@ -0,0 +1,92 @@
+// Copyright 2025 the github.com/koonix/x authors.
+// SPDX-License-Identifier: Apache-2.0
+
+package ioutil
+
+import (
+	"io"
+)
+
+type FooterReader struct {
+	r          io.Reader
+	footer     []byte
+	footerSize int
+	extra      []byte
+}
+
+// NewFooterReader returns a reader that reads data from r,
+// excluding the last len(footer) bytes before EOF,
+// which can be retrieved via [FooterReader.Footer].
+//
+// Optionally, an extra buffer with the same length as footer
+// can be provided to prevent allocation for the internal operations.
+//
+// [FooterReader.Footer] always returns the latest read bytes,
+// regardless of errors or whether EOF has been achieved.
+//
+// [FooterReader.Read] is safe to call again
+// after errors (including EOF).
+func NewFooterReader(r io.Reader, footer []byte, extra ...[]byte) *FooterReader {
+	f := &FooterReader{
+		r:      r,
+		footer: footer,
+	}
+	if len(extra) > 0 {
+		if len(extra[0]) != len(footer) {
+			panic("NewFooterReader: len(extra[0]) != len(footer)")
+		}
+		f.extra = extra[0]
+	}
+	return f
+}
+
+func (r *FooterReader) Read(b []byte) (int, error) {
+
+	e := 0
+	for r.footerSize != len(r.footer) {
+		x := len(r.footer) - r.footerSize
+		n, err := r.r.Read(r.footer[:x])
+		copy(r.footer[x-n:x], r.footer[:n])
+		r.footerSize += n
+		if err == io.EOF {
+			return 0, io.ErrUnexpectedEOF
+		}
+		if err != nil {
+			return 0, err
+		}
+		if n == 0 {
+			e++
+		} else {
+			e = 0
+		}
+		if e >= maxConsecutiveEmptyReads {
+			return 0, io.ErrNoProgress
+		}
+	}
+
+	n, err := r.r.Read(b)
+
+	if n == 0 {
+		return n, err
+	}
+
+	if r.extra == nil {
+		r.extra = make([]byte, len(r.footer))
+	}
+	copy(r.extra, r.footer)
+
+	f := len(r.footer)
+	x := min(n, f)
+
+	f -= copy(r.footer[f-x:f], b[n-x:n])
+	copy(r.footer[:f], r.extra[len(r.extra)-f:])
+
+	copy(b[x:n], b[:n-x])
+	copy(b[:x], r.extra[:len(r.extra)-f])
+
+	return n, err
+}
+
+func (r *FooterReader) Footer() []byte {
+	return r.footer[len(r.footer)-r.footerSize:]
+}
---
+++ ioutil/footer_reader_test.go
@@ -0,0 +1,104 @@
+// Copyright 2025 the github.com/koonix/x authors.
+// SPDX-License-Identifier: Apache-2.0
+
+package ioutil_test
+
+import (
+	"fmt"
+	"io"
+	"runtime"
+	"strings"
+	"testing"
+
+	"github.com/koonix/x/ioutil"
+)
+
+func TestFooterReader(t *testing.T) {
+
+	type read struct {
+		line    int
+		len     int
+		content string
+		err     error
+	}
+
+	tests := []struct {
+		line      int
+		footerLen int
+		stream    string
+		footer    string
+		reads     []read
+	}{
+		{
+			line(),
+			10,
+			"the quick brown fox jumps over",
+			"jumps over",
+			[]read{
+				{
+					line(),
+					15,
+					"the quick brown",
+					nil,
+				},
+				{
+					line(),
+					5,
+					" fox ",
+					nil,
+				},
+				{
+					line(),
+					3,
+					"",
+					io.EOF,
+				},
+			},
+		},
+	}
+
+	for i, test := range tests {
+		t.Run(fmt.Sprintf("test%d-line%d", i, test.line), func(t *testing.T) {
+
+			r := ioutil.NewFooterReader(strings.NewReader(test.stream), make([]byte, test.footerLen))
+
+			for i, read := range test.reads {
+				t.Run(fmt.Sprintf("read%d-line%d", i, read.line), func(t *testing.T) {
+
+					b := make([]byte, read.len)
+					n, err := r.Read(b)
+					content := string(b[:n])
+
+					if read.content != content {
+						t.Errorf(
+							"incorrect read content: want %q, got %q",
+							read.content, content,
+						)
+					}
+
+					if read.err != err {
+						t.Errorf(
+							"incorrect error: want %v, got %v",
+							read.err, err,
+						)
+					}
+				})
+			}
+
+			want := test.footer
+			got := string(r.Footer())
+			if want != got {
+				t.Errorf(
+					"incorrect footer: want %q, got %q",
+					want, got,
+				)
+			}
+		})
+	}
+}
+
+// line returns the line number where it's called.
+func line() int {
+	_, _, line, _ := runtime.Caller(1)
+	return line
+}
